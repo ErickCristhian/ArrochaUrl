121d51b5744891fa877b25116b09b7a9fcc6dcbb
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2021-07-22 20:59:50 -0300

    Configurado CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,12 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+})
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
